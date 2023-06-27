2c7fc087f2729d43e9e2fcedcd2f18eb25f790d6
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-27 06:42:47 +0100

    done with 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * free_dog - free struct dog
+ * @d: struct dog pointer
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	if (!d->owner)
+		free(d->owner);
+
+	if (!d->name)
+		free(d->name);
+
+	free(d);
+}
