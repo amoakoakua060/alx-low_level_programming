6fbc2507488d7ab50690501b027e020a9f9ab635
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-27 05:16:25 +0100

    done with 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,1 +1,26 @@
- 
+#include "main.h"
+
+/**
+ * _calloc - allocate
+ * @nmemb: integer
+ * @size: integer
+ * Return: void *
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (; i < nmemb * size; i++)
+		*(ptr + i) = 0;
+
+	return ((void *) ptr);
+}
