26b33e504077997a84527a46bbed1aa6949c362b
Ekua Amoako <amoakoakua060.gmail.com>
2023-08-07 14:45:11 -0700

    done with task 4

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,4 +14,15 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+void check_elf(Elf64_Ehdr *fh);
+void print_magic(Elf64_Ehdr *fh);
+void print_class(Elf64_Ehdr *fh);
+void print_data(Elf64_Ehdr *fh);
+void print_version(Elf64_Ehdr *fh);
+void print_osabi(Elf64_Ehdr *fh);
+void print_abiversion(Elf64_Ehdr *fh);
+void print_type(Elf64_Ehdr *fh);
+void print_entry(Elf64_Ehdr *fh);
+void close_elf(int elf);
+
 #endif /* MAIN_H */
