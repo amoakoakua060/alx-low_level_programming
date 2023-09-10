fc1ffd4c64ed7bca1038f67e0770acff9fdb2411
Ekua Amoako <amoakoakua060.gmail.com>
2023-09-09 23:10:56 -0700

    done with task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_create - create a hash table
+* @size: the size of the created hash table
+*
+* Return: hash_table *
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int a = 0;
+	hash_table_t *hash_t = NULL;
+
+	if (size == 0)
+		return (NULL);
+
+	hash_t = malloc(sizeof(hash_table_t));
+	if (hash_t == NULL)
+		return (NULL);
+
+	hash_t->size = size;
+	hash_t->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_t->array == NULL)
+	{
+		free(hash_t);
+		return (NULL);
+	}
+
+	while (i < size)
+	{
+		hash_t->array[i] = NULL;
+		i++;
+	}
+
+	return (hash_t);
+}
